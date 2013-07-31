051f66f1e98c482e81ed835151cc5534f7c389fe
e0d <edward@indeterminate.org>
2013-07-31 16:57:48 -0400

    cleanup

--- cms/envs/aws_migrate.py
+++ cms/envs/aws_migrate.py
@@ -1,6 +1,6 @@
 """
-A Django settings file for use on AWS while running 
-database migrations, since we don't want to normally run the 
+A Django settings file for use on AWS while running
+database migrations, since we don't want to normally run the
 LMS with enough privileges to modify the database schema.
 """
 
@@ -17,8 +17,8 @@ USER = os.environ.get('DB_MIGRATION_USER', 'root')
 PASSWORD = os.environ.get('DB_MIGRATION_PASS', None)
 
 if not PASSWORD:
-   raise ImproperlyConfigured("No database password was provided for running "
-                              "migrations.  This is fatal.")
+    raise ImproperlyConfigured("No database password was provided for running "
+                               "migrations.  This is fatal.")
 
 DATABASES['default']['USER'] = USER
 DATABASES['default']['PASSWORD'] = PASSWORD
